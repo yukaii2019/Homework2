8bdc99032d01ec7f1050fe716e1431b370063091
Yukai Hsieh <yukai09151702@gmail.com>
2020-03-28 00:26:04 +0800

    add main.cpp revise

--- main.cpp
+++ main.cpp
@@ -12,10 +12,6 @@ char table_fp[11] = {0xBF, 0x86, 0xDB, 0xCF, 0xE6, 0xED, 0xFD, 0x87, 0xFF, 0xEF,
 int sample = 1024;
 float ADCdata[1024];
 
-typedef struct POINT{
-    float V;
-    float t;
-} POINT;
 int main(){
     for(int i=0;i<sample ;i++){
         ADCdata[i] = Ain;
@@ -25,7 +21,7 @@ int main(){
         pc.printf("%1.3f\r\n",ADCdata[i]);
         wait(0.01);
     }
-    POINT maxpos[200];
+    float maxpos[200];
     float max = 0;
     float maxfront = 0;
     float maxnext = 0;
@@ -35,45 +31,42 @@ int main(){
         maxfront = ADCdata[i-1];
         maxnext = ADCdata[i+1];
         if(max>maxfront&&max>maxnext){
-            maxpos[pos].V = max;
-            maxpos[pos].t = i*(1./(sample)); 
+            maxpos[pos] = i*(1./(sample)); 
             pos++;
         }
     }
-    
-    for(int i =0;i<pos;i++){
-        pc.printf("%1.8f %1.8f\r\n",maxpos[i].t,maxpos[i].V);
-    }
-    float sum=0;
-    for(int i=1;i<pos;i++){
-        sum+=(maxpos[i].t-maxpos[i-1].t);
-    }
+    float sum = maxpos[pos-1]-maxpos[0];
     float avg = sum*1./(pos-1);
-    pc.printf("%1.8f\r\n",avg);
+   // pc.printf("%1.8f\r\n",avg);
     float avg_freq = 1./avg;
-    pc.printf("%1.8f\r\n",avg_freq);
-    int freq_display_on_ssd[3];
+  //  pc.printf("%1.8f\r\n",avg_freq);
     int round_freq = (int)(avg_freq+0.5);
-    pc.printf("%d\r\n",round_freq);
+    int freq_display_on_ssd[3];
+   // pc.printf("%d\r\n",round_freq);
     int tmp_round_freq =round_freq; 
     for(int i=0;i<3;i++){
         freq_display_on_ssd[i] = tmp_round_freq%10;
         tmp_round_freq/=10;
     }
-    for(int i=0;i<3;i++){
-        pc.printf("%d\r\n",freq_display_on_ssd[i]);
-    }
+    //for(int i=0;i<3;i++){
+    //    pc.printf("%d\r\n",freq_display_on_ssd[i]);
+    //}
     float delay = 1./(round_freq*25);
     while(1){
-        if(Switch ==0){
+        if(Switch == 0){
             greenLED = 1;
             redLED = 0;
-            display = table[freq_display_on_ssd[2]];
-            wait (1);
-            display = table[freq_display_on_ssd[1]];
-            wait (1);
-            display = table_fp[freq_display_on_ssd[0]];
-            wait (1);
+            while(1){
+                display = table[freq_display_on_ssd[2]];
+                wait (1);
+                if(Switch == 1)break;
+                display = table[freq_display_on_ssd[1]];
+                wait (1);
+                if(Switch == 1)break;
+                display = table_fp[freq_display_on_ssd[0]];
+                wait (1);
+                if(Switch == 1)break;
+            }
         }
         else{
             greenLED = 0;
